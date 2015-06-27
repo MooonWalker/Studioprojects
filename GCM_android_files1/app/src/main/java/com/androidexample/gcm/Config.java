650922344a9ee8621eb2c02b7757bb8af6153571
Moon Walker <almodook@freemail.hu>
2015-06-27 13:01:51 +0200

    2

---
+++ GCM_android_files1/app/src/main/java/com/androidexample/gcm/Config.java
@@ -0,0 +1,20 @@
+package com.androidexample.gcm;
+
+public interface Config
+{
+	// CONSTANTS
+	static final String YOUR_SERVER_URL =  "http://somejourney.info/gcm_server_files/register.php";
+	// YOUR_SERVER_URL : Server url where you have placed your server files
+    // Google project id
+    static final String GOOGLE_SENDER_ID = "865494874332";  // Place here your Google project id
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCM Android Example";
+    static final String DISPLAY_MESSAGE_ACTION =
+            "com.androidexample.gcm.DISPLAY_MESSAGE";
+    static final String EXTRA_MESSAGE = "message";
+		
+	
+}
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+//Database config
+
+define('DB_DATABASE', '09165_some_wp');
+/** MySQL database username */
+define('DB_USER', '09165_some_wp');
+/** MySQL database password */
+define('DB_PASSWORD', 'Flight58');
+/** MySQL hostname */
+define('DB_HOST', 'mysql.viacomkft.hu');
+// Google Cloud Messaging API Key
+// Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyCYV-SHgo3OoU6fj-gvT5Bj8JmmOFZDEz4"); 
+?>
